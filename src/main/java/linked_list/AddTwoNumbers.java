39e9aa2a71714ec951d0b1253e0576c265cc81bb
makc2299 <44743531+makc2299@users.noreply.github.com>
2023-11-16 23:31:48 +0200

    Add Two Numbers

---
+++ src/main/java/linked_list/AddTwoNumbers.java
@@ -0,0 +1,63 @@
+/**
+ You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+ and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ Input: l1 = [2,4,3], l2 = [5,6,4]
+ Output: [7,0,8]
+ Explanation: 342 + 465 = 807.
+ Example 2:
+
+ Input: l1 = [0], l2 = [0]
+ Output: [0]
+ Example 3:
+
+ Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ Output: [8,9,9,9,0,0,0,1]
+
+ Constraints:
+ The number of nodes in each linked list is in the range [1, 100].
+ 0 <= Node.val <= 9
+ It is guaranteed that the list represents a number that does not have leading zeros.
+
+ */
+
+package main.java.linked_list;
+
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode output = new ListNode();
+        ListNode pointer = output;
+        int val1, val2, sum, val3 = 0;
+
+        do {
+            val1 = 0;
+            if (l1 != null) {
+                val1 = l1.val;
+                l1 = l1.next;
+            }
+
+            val2 = 0;
+            if (l2 != null) {
+                val2 = l2.val;
+                l2 = l2.next;
+            }
+
+            sum = val1 + val2 + val3;
+            val3 = sum / 10;
+            pointer.val = val3 != 0 ? sum % 10 : sum;
+
+            if (l1 == null && l2 == null && val3 == 0) {
+                break;
+            }
+
+            pointer.next = new ListNode();
+            pointer = pointer.next;
+
+        } while (true);
+
+        return output;
+    }
+}
+
