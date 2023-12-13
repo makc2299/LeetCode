e81a9ab2df49c73a6791460d60621685e7edbed4
makc2299 <44743531+makc2299@users.noreply.github.com>
2023-12-13 16:37:28 +0200

    Letter Combinations of a Phone Number

---
+++ src/main/java/math/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,58 @@
+/**
+ Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+ A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+ Example 1:
+
+ Input: digits = "23"
+ Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ Example 2:
+
+ Input: digits = ""
+ Output: []
+ Example 3:
+
+ Input: digits = "2"
+ Output: ["a","b","c"]
+
+ Constraints:
+
+ 0 <= digits.length <= 4
+ digits[i] is a digit in the range ['2', '9'].
+ */
+
+package main.java.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombinationsOfAPhoneNumber {
+
+    String[] strs;
+    {
+        strs = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    }
+    List<String> res;
+    public List<String> letterCombinations(String digits) {
+        res = new ArrayList<>();
+        if (!digits.isEmpty()) {
+            StringBuilder stringBuilder = new StringBuilder();
+            combine(stringBuilder, digits);
+        }
+        return res;
+    }
+
+    private void combine(StringBuilder sb, String digits) {
+        if (digits.isEmpty()) {
+            res.add(sb.toString());
+        } else {
+            String str = strs[Character.getNumericValue(digits.charAt(0))];
+            for (int i = 0; i < str.length(); i++) {
+                sb.append(str.charAt(i));
+                combine(sb, digits.substring(1));
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+    }
+}
