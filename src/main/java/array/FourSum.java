eb10ede743ae309c07a5cafb813f4127e109ed7f
makc2299 <44743531+makc2299@users.noreply.github.com>
2023-12-14 11:29:11 +0200

    4Sum

---
+++ src/main/java/array/FourSum.java
@@ -0,0 +1,71 @@
+/**
+ Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+ 0 <= a, b, c, d < n
+ a, b, c, and d are distinct.
+ nums[a] + nums[b] + nums[c] + nums[d] == target
+ You may return the answer in any order.
+
+ Example 1:
+
+ Input: nums = [1,0,-1,0,-2,2], target = 0
+ Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ Example 2:
+
+ Input: nums = [2,2,2,2,2], target = 8
+ Output: [[2,2,2,2]]
+
+ Constraints:
+
+ 1 <= nums.length <= 200
+ -109 <= nums[i] <= 109
+ -109 <= target <= 109
+ */
+
+package main.java.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        long sum;
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int k = j + 1;
+                int l = nums.length - 1;
+                while (k < l) {
+                    sum = nums[i] + nums[j];
+                    sum += nums[k] + nums[l];
+                    if (sum < target) {
+                        k++;
+                    } else if (sum > target) {
+                        l--;
+                    } else {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        int lastK = nums[k], lastL = nums[l];
+                        while (k < l && nums[k] == lastK) {
+                            k++;
+                        }
+                        while (l > k && nums[l] == lastL) {
+                            l--;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
