62a1f0c0d66af328ceecf2252ba087f15ad03529
makc2299 <44743531+makc2299@users.noreply.github.com>
2023-12-25 16:33:13 +0200

    Find the Index of the First Occurrence in a String

---
+++ src/main/java/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,63 @@
+/**
+ Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ Example 1:
+
+ Input: haystack = "sadbutsad", needle = "sad"
+ Output: 0
+ Explanation: "sad" occurs at index 0 and 6.
+ The first occurrence is at index 0, so we return 0.
+ Example 2:
+
+ Input: haystack = "leetcode", needle = "leeto"
+ Output: -1
+ Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+ Constraints:
+
+ 1 <= haystack.length, needle.length <= 104
+ haystack and needle consist of only lowercase English characters.
+ */
+
+package main.java;
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+
+    public int strStr1(String haystack, String needle) {
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i < haystack.length(); i++) {
+            a : if (haystack.charAt(i) == needle.charAt(0)) {
+                for (int j = 1; j < needle.length(); j++) {
+                    if (i + j == haystack.length() || haystack.charAt(i + j) != needle.charAt(j)) {
+                        break a;
+                    }
+                }
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int strStr(String haystack, String needle) {
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+
+        int haystackLen = haystack.length();
+        int needleLen = needle.length();
+        int i = 0, j = 0;
+        for (; i < haystackLen && j < needleLen; i++) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            } else {
+                i -= j;
+                j = 0;
+            }
+        }
+
+        return j == needle.length() ? i - j : -1;
+    }
+}
