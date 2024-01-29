2241042168d7005a514bd1d9576dce24c76540ab
makc2299 <44743531+makc2299@users.noreply.github.com>
2024-01-29 23:10:30 +0200

    Unique Paths II

---
+++ src/main/java/dynamic_programming/UniquePathsII.java
@@ -0,0 +1,54 @@
+/**
+ You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+ An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+ Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+ The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+ Example 1:
+
+ Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+ Output: 2
+ Explanation: There is one obstacle in the middle of the 3x3 grid above.
+ There are two ways to reach the bottom-right corner:
+ 1. Right -> Right -> Down -> Down
+ 2. Down -> Down -> Right -> Right
+ Example 2:
+
+ Input: obstacleGrid = [[0,1],[0,0]]
+ Output: 1
+
+ Constraints:
+
+ m == obstacleGrid.length
+ n == obstacleGrid[i].length
+ 1 <= m, n <= 100
+ obstacleGrid[i][j] is 0 or 1.
+ */
+
+package main.java.dynamic_programming;
+
+public class UniquePathsII {
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+
+        if (obstacleGrid[m - 1][n - 1] == 0) {
+            dp[m - 1][n - 1] = 1;
+        }
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] += dp[i][j + 1] + dp[i + 1][j];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
